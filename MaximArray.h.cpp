cae5e67d3a396e98b9c537edf483ec1ff899ca3e
София Митрофанова <90389854+tamaladie@users.noreply.github.com>
2022-06-27 23:38:39 +0300

    Create MaximArray.h.cpp

---
+++ MaximArray.h.cpp
@@ -0,0 +1,8 @@
+#ifndef __ARRAY_H__ // Конструкция include guard 
+#define __ARRAY_H__
+
+// Сигнатура функций
+void print_array(int *arr, size_t len);
+void fill_random(int *arr, size_t len);
+
+#endif // __ARRAY_H__
