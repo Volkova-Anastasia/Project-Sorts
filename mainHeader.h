3540c2a699a119967ca688d8b4bf0633f4335c03
Stasyavlgg <90391279+Volkova-Anastasia@users.noreply.github.com>
2022-06-28 04:01:33 +0300

    Create mainHeader.h

---
+++ mainHeader.h
@@ -0,0 +1,16 @@
+//
+// Created by nedod on 28.06.2022.
+//
+#pragma once
+#include "Grafic.cpp"
+#include "Masha.cpp"
+#include "SofiaBucket.cpp"
+#include <string>
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <cstdlib>
+#ifndef UNTITLED4_MAINHEADER_H
+#define UNTITLED4_MAINHEADER_H
+
+#endif //UNTITLED4_MAINHEADER_H
